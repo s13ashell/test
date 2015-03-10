73878adeb60c237b4526ae8cf1c447b862744aff
Xia, Zhiyu <xiazhiyumail@gmail.com>
2015-03-10 11:44:40 +0800

    A modification of the source file

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ using namespace std;
 int main()
 {
     for (int i = 0; i < 10; i ++) printf("I'm no. %d member ~\n", i);
+    for (int i = 0; i < 10; i ++) printf("I'm no. %d member's girl friend ~\n");
     return 0;
 }

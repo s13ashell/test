0f3218f0a9c70c1bd558e7b71e8464579b6c6fec
Xia, Zhiyu <xiazhiyumail@gmail.com>
2015-03-13 16:06:15 +0800

    some words ...

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <cstdio>
 #include <iostream>
 
-using namespace std;
+using namespace std;"
 
 int main()
 {
     for (int i = 0; i < 10; i ++) printf("I'm no. %d member ~\n", i);
     for (int i = 0; i < 10; i ++) printf("I'm no. %d member's girl friend ~\n");
     for (int i = 0; i < 10; i ++) printf("I'm no. %d member's girl friend's father ~\n");
+    printf("Salaam !");
+	printf("Another word ...");
     return 0;
 }
